9bad620e651dcf71c4ee2afaa1ff6e3e97bb7cb9
Bruce Durling <bld@otfrom.com>
2010-09-05 11:49:53 +0100

    Basic data for chapter 1.

--- src/ciscratch/core.clj
+++ src/ciscratch/core.clj
@@ -1,1 +1,38 @@
 (ns ciscratch.core)
+
+(def critics {"Lisa Rose"  {"Lady in the Water"  2.5
+                            "Snakes on a Plane"  3.5
+                            "Just My Luck"  3.0
+                            "Superman Returns"  3.5
+                            "You, me and Dupree"  2.5
+                            "The Night Listener"  3.0}
+              "Gene Seymour"  {"Lady in the Water"  3.0
+                               "Snakes on a Plane"  3.5
+                               "Just My Luck"  1.5
+                               "Superman Returns"  5.0
+                               "You, me and Dupree"  3.5
+                               "The Night Listener"  3.0}
+              "Michael Phllips"  {"Lady in the Water"  2.5
+                                  "Snakes on a Plane"  3.0
+                                  "Superman Returns"  3.5
+                                  "The Night Listener"  4.0}
+              "Claudia Puig"  {"Lady in the Water"  3.0
+                               "Snakes on a Plane"  4.0
+                               "Just My Luck"  2.0
+                               "Superman Returns"  4.0
+                               "You, me and Dupree"  2.0}
+              "Mick LaSalle"  {"Lady in the Water"  3.0
+                               "Snakes on a Plane"  4.0
+                               "Just My Luck"  2.0
+                               "Superman Returns"  3.0
+                               "You, me and Dupree"  2.0
+                               "The Night Listener"  3.0}
+              "Jack Mathews"  {"Lady in the Water"  3.0
+                               "Snakes on a Plane"  4.0
+                               "Superman Returns"  5.0
+                               "You, me and Dupree"  3.5
+                               "The Night Listener"  3.0}
+              "Toby"  {"Snakes on a Plane"  4.5
+                       "Superman Returns"  4.0
+                       "You, me and Dupree"  1.0}})
+
