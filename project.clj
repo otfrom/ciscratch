4ff71d3f6b4f6a991190d9ae884ad8c1c425abb3
Bruce Durling <bld@otfrom.com>
2010-09-05 11:49:39 +0100

    Added basics for the project (Incanter and Swank).

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject ciscratch "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject ciscratch "0.0.1-SNAPSHOT"
+  :description "A scratch area to explore O'Reilly's Collective Intelligence."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [incanter "1.2.3"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
